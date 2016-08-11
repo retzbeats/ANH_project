a31c5f8a5f5270b55ae6ce7564774be6970c2e0c
Maria Fernanda Chaparro Niño <chaparro-m@javeriana.edu.co>
2016-08-11 18:13:03 -0500

    Comments Mafe Servidor

--- libmodbus/random-test-server.c
+++ libmodbus/random-test-server.c
@@ -15,12 +15,12 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include <stdio.h> */ (Stadard Input/Output Header) Contiene las definiciones necesarias para las operaciones de las in/out*/ 
-#include <unistd.h> */Heade File que provee acceso al API POSIX de los sistemas UNIX (Base de las especificaciones unicas de UNIX)*/
-#include <stdlib.h> */ (Standard Library).Estándar de propósito general de C que contiene las funciones para gestión de memoria dinámica, control de procesos y otras*/
-#include <errno.h> */Librerias de C que define los macros para los informes de códigos*/
+#include <stdio.h> /* (Stadard Input/Output Header) Contiene las definiciones necesarias para las operaciones de las in/out*/ 
+#include <unistd.h> /* Heade File que provee acceso al API POSIX de los sistemas UNIX (Base de las especificaciones unicas de UNIX)*/
+#include <stdlib.h> /* (Standard Library).Estándar de propósito general de C que contiene las funciones para gestión de memoria dinámica, control de procesos y otras*/
+#include <errno.h> /* Librerias de C que define los macros para los informes de códigos*/
 
-#include <modbus.h> */Libreria de modbus*/
+#include <modbus.h> /*Libreria de modbus*/
 
 int main(void)
 {
@@ -45,10 +45,10 @@ int main(void)
     modbus_tcp_accept(ctx, &socket);
 
     for (;;) {
-        uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH]; */Type: unsigned char, busca registro que correspondan a la máxima longitud del paquete Modbus TCP/IP */
-        int rc; */Declaración rc de tipo entero*/
+        uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH]; /*Type: unsigned char, busca registro que correspondan a la máxima longitud del paquete Modbus TCP/IP */
+        int rc; /*Declaración rc de tipo entero*/
 
-        rc = modbus_receive(ctx, query);
+        rc = modbus_receive(ctx, query); /**/
         if (rc != -1) {
             /* rc is the query size */
             modbus_reply(ctx, query, rc, mb_mapping);
